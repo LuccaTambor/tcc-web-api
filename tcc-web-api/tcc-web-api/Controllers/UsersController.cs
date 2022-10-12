9891ec2ab16944f93bfcdd00e10ad356eb0656a6
LuccaTambor <lucca.tambor@rotadasoficinas.com.br>
2022-10-11 21:12:25 -0300

    feat: Add UsersController
    Created the usersController and create a Post mehtod to create a new user

---
+++ tcc-web-api/tcc-web-api/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using tcc_web_api.Data;
+using tcc_web_api.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace tcc_web_api.Controllers {
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase {
+        private TCCDbContext _context;
+
+        public UsersController(TCCDbContext context) {
+            _context = context;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("createFirstUser")]
+        public IActionResult CreateUser() {
+            User newUser = new User {
+                Name = "Lucca Tambor",
+                UserName = "tambor",
+                Password = "senha123",
+                ConfirmPassword = "senha123",
+                LockoutEnabled = false,
+                NormalizedUserName = "tambor",
+                Email = "lucca.tambor@unesp.br",
+                EmailConfirmed = true,
+            };
+
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
