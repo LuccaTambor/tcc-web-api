9d5f5dece55d8869023b622403bfcc23e2434f8c
LuccaTambor <lucca.tambor@outlook.com>
2022-10-31 09:31:43 -0300

    feat: methods for developer

--- tcc-web-api/tcc-web-api/Controllers/ProjectsController.cs
+++ tcc-web-api/tcc-web-api/Controllers/ProjectsController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using tcc_web_api.Data;
@@ -7,6 +7,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Net.Http.Json;
 using Newtonsoft.Json;
+using System.Linq;
 
 namespace tcc_web_api.Controllers {
     [Route("api/projects")]
@@ -41,12 +42,48 @@ namespace tcc_web_api.Controllers {
         }
 
         [HttpGet]
+        [Route("getProjectAsDev")]
+        public IActionResult GetProjectAsDev(int id, string devId) {
+            var proj = _context.Projects.Where(p => p.Id == id).Select(p => new {
+                p.Id,
+                p.Description,
+                p.StartedOn,
+                p.ExpectedFinishDate,
+                Teams = p.Teams.Where(t => t.Developers.Any(d => d.Id == devId)).Select(t => new {
+                    t.Id,
+                    t.TeamName
+                })
+            }).FirstOrDefault();
+
+            return Ok(proj);
+        }
+
+        [HttpGet]
         [AllowAnonymous]
         [Route("GetManagerProjects")]
         public IActionResult GetProjectsFromManager(string id) {
             var result = _context.Projects.Where(m => m.Manager.Id == id).Include(p => p.Teams);
 
             return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("GetDevProjects")]
+        public IActionResult GetProjectsFromDev(string id) {
+            var teamsIdsDev = _context.Developers.Include(d => d.Teams).FirstOrDefault(d => d.Id == id).Teams.Select(t => t.Id);
+
+            var projs = _context.Projects.Where(p => p.Teams.Select(t => t.Id).Any(v => teamsIdsDev.Contains(v))).Select(p => new {
+                p.Id,
+                p.Description,
+                p.StartedOn,
+                p.ExpectedFinishDate,
+                Teams = p.Teams.Select(t => new {
+                    t.Id,
+                    t.TeamName
+                })
+            });
+
+            return Ok(projs);
         }
     }
 }
